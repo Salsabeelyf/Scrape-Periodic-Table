cd2a9f00cd36ee52fd2994f6436e7077801431d0
Salsabeelyf <salsabeelsay@gmail.com>
2024-08-30 10:19:41 +0100

    Get all elements and output as required

---
+++ Scrape_Periodic_Table/pipelines.py
@@ -0,0 +1,39 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+import sqlite3
+from itemadapter import ItemAdapter
+
+
+class ScrapePeriodicTablePipeline:
+    def process_item(self, item, spider):
+        return item
+
+
+class SaveToDatabasePipeline:
+    def __init__(self):
+        self.con = sqlite3.connect('periodic_table.db')
+        self.cur = self.con.cursor()
+
+    def open_spider(self, spider):
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS periodic_table
+                         (element_symbol TEXT PRIMARY KEY,
+                         element_name TEXT,
+                         atomic_number INTEGER,
+                         atomic_mass REAL,
+                         chemical_group TEXT)
+                         """)
+        self.con.commit()
+
+    def process_item(self, item, spider):
+        self.con.execute("""
+                             INSERT INTO periodic_table (element_symbol, element_name, atomic_number, atomic_mass, chemical_group) VALUES (?,?,?,?,?)""",
+                             (item['symbol'], item['name'], item['atomic_number'], item['atomic_mass'], item['chemical_group']))
+        self.con.commit()
+
+    def close_spider(self, spider):
+        self.con.close()
---
+++ Scrape_Periodic_Table/spiders/elements_spider.py
@@ -0,0 +1,71 @@
+import scrapy
+from scrapy import Request
+from scrapy_playwright.page import PageMethod
+
+
+class ElementsSpiderSpider(scrapy.Spider):
+    name = "elements_spider"
+    allowed_domains = ["nih.gov"]
+    start_urls = ["https://pubchem.ncbi.nlm.nih.gov/ptable/"]
+    elements = {}
+
+    def start_requests(self):
+        yield Request(self.start_urls[0],
+                            meta=dict(
+                            playwright= True,
+                            playwright_page_methods=[
+                                PageMethod('wait_for_load_state',"domcontentloaded"),
+                                PageMethod('wait_for_function', "document.querySelector('div.element') !== null")
+                                ]
+                        ))
+
+    def parse(self, response):
+        for e in response.css('div.element'):
+            chemical_group = e.css('div[data-tooltip="Chemical Group Block"] span::text').get()
+            if chemical_group not in self.elements.keys():
+                self.elements[chemical_group] = {
+                    'element_count': 0,
+                    'elements': []
+                }
+            self.elements[chemical_group]['element_count'] += 1
+            self.elements[chemical_group]['elements'].append({
+                'symbol': e.css('div[data-tooltip="Symbol"]::text').get(),
+                'name': e.css('div[data-tooltip="Name"]::text').get(),
+                'atomic_mass': float(e.css('div[data-tooltip="Atomic Mass, u"]::text').get()),
+                'atomic_number': int(e.css('div[data-tooltip="Atomic Number"]::text').get())
+            })
+
+        yield self.elements
+
+
+
+# Exctract:
+# symbol div[data-tooltip="Symbol"]::text
+# name div[data-tooltip="Name"]::text
+# atomic mass (float) div[data-tooltip="Atomic Mass, u"]::text
+# atomic number (int)  div[data-tooltip="Atomic Number"]::text
+# chemical group div[data-tooltip="Chemical Group Block"] span::text
+
+
+# Save data into database
+
+# output data as a nested dictionary grouped by chemical group
+# {
+#     "nonmetal": {
+#         "element_count": 7,
+#         "elements": [
+#             {
+#                 "symbol": "H",
+#                 "name": "Hydrogen",
+#                 "atomic_number": 1,
+#                 "atomic_mass": 1.008
+#             },
+#             {
+#                 "symbol": "H",
+#                 "name": "Hydrogen",
+#                 "atomic_number": 1,
+#                 "atomic_mass": 1.008
+#             }
+#         ]
+#     }
+# }
